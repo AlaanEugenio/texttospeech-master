92f301a2015e51c620e352c53c19d6fc86185bdc
AlaanEugenio <alaneugenio.vergara@alumnos.ulagos.cl>
2025-02-05 16:16:38 -0300

    cambios a index.php y registro.php 5 febrero 2025

---
+++ registro.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Formulario de Registro SCIII</title>
+<link href="estilos.css" rel="stylesheet" type="text/css">
+</head>
+
+
+
+
+
+</html>
+
+
+
